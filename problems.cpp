e9cb6d514df2ab5c69452e78a32ec3cfbd8d1ede
SAMarSAMir2 <102358763+SAMarSAMir2@users.noreply.github.com>
2023-04-20 14:46:48 +0200

    generateBin, sortQueue

--- problems.cpp
+++ problems.cpp
@@ -1,11 +1,19 @@
 //Combine Nodes Between Zeros
 #include "DoublyLinkedList.cpp"
+#include "Queue.cpp"
 
 template <class T>
 void CombineNodesBetweenZeros(DoublyLinkedList<T> *dll);
 
 template <class T>
 DoublyLinkedList<T>* MergeKsortedlinkedlists(DoublyLinkedList<T> **dll,int k);
+
+// template <class T>
+// SingleLinkedList<T> generateBinaryNumbers(int n);
+
+// template <class T>
+// void sortQueue(Queue<T> &q);
+
 
 int main(){
     // testing Combine Nodes Between Zeros
@@ -40,6 +48,23 @@ int main(){
     // dll[2] = dll3;
     // DoublyLinkedList<int> *result = MergeKsortedlinkedlists(dll,3);
     // result->forwardTraversal();
+
+    //  generate binary numbers
+    // SingleLinkedList<string> binaryNumbers = generateBinaryNumbers<string>(10);
+    // for (int i = 0; i < binaryNumbers.linkedListSize(); i++)
+    // {
+    //     cout << binaryNumbers.retrieveAt(i) << " ";
+    // }
+    // return 0;
+
+    // sort queue
+    // Queue<int> q;
+    // q.enqueue(3);
+    // q.enqueue(15);
+    // q.enqueue(2);
+    // q.enqueue(4);
+    // sortQueue(q);
+    // q.print();
 }
 
 
@@ -85,3 +110,51 @@ DoublyLinkedList<T>* MergeKsortedlinkedlists(DoublyLinkedList<T> **dll, int k) {
     }
     return result;
 }
+
+
+
+// template <class T>
+// SingleLinkedList<T> generateBinaryNumbers(int n)
+// {
+//     Queue<T> q;
+//     SingleLinkedList<T> binaryNumbers;
+//     q.enqueue("1"); // Enqueue a string
+
+//     while (binaryNumbers.linkedListSize() < n)
+//     {
+//         T curr = q.first();
+//         q.dequeue();
+//         binaryNumbers.insertAtTail(curr);
+//         T s1 = curr + "0";
+//         T s2 = curr + "1";
+//         q.enqueue(s1);
+//         q.enqueue(s2);
+//     }
+
+//     return binaryNumbers;
+// }
+
+// template <class T>
+// void sortQueue(Queue<T> &q)
+// {
+//     int size = q.queueSize();
+//     for (int i = 0; i < size; i++)
+//     {
+//         T min = q.dequeue();
+//         for (int j = 0; j < size - i - 1; j++)
+//         {
+//             T temp = q.dequeue();
+//             if (temp < min)
+//             {
+//                 q.enqueue(min);
+//                 min = temp;
+//             }
+//             else
+//             {
+//                 q.enqueue(temp);
+//             }
+//         }
+//         q.enqueue(min);
+//     }
+// }
+
