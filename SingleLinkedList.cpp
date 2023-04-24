4a87252e6012c3d82dcd12a2d4b8efabf6796dac
SAMarSAMir2 <102358763+SAMarSAMir2@users.noreply.github.com>
2023-04-24 11:32:41 +0200

    update function swap SLL

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -204,23 +204,75 @@ bool SingleLinkedList<T>::isItemAtEqual(T element, int index)
     return false;
 }
 
+// template <class T>
+// void SingleLinkedList<T>::swap(int firstItemIdx, int secondItemIdx)
+// {
+//     Node<T> *temp = head;
+//     for (int i = 0; i < firstItemIdx; i++)
+//     {
+//         temp = temp->next;
+//     }
+//     Node<T> *temp2 = head;
+//     for (int i = 0; i < secondItemIdx; i++)
+//     {
+//         temp2 = temp2->next;
+//     }
+//     T tempvalue = temp->value;
+//     temp->value = temp2->value;
+//     temp2->value = tempvalue;
+// }
+
 template <class T>
-void SingleLinkedList<T>::swap(int firstItemIdx, int secondItemIdx)
-{
-    Node<T> *temp = head;
-    for (int i = 0; i < firstItemIdx; i++)
-    {
-        temp = temp->next;
+void SingleLinkedList<T>::swap(int firstItemIdx, int secondItemIdx) {
+    // If both indices are the same, there is nothing to swap
+    if (firstItemIdx == secondItemIdx) {
+        return;
     }
-    Node<T> *temp2 = head;
-    for (int i = 0; i < secondItemIdx; i++)
-    {
-        temp2 = temp2->next;
+    
+    // Find the first node to be swapped
+    Node<T>* prev1 = nullptr;
+    Node<T>* curr1 = head;
+    int i = 0;
+    while (curr1 != nullptr && i < firstItemIdx) {
+        prev1 = curr1;
+        curr1 = curr1->next;
+        i++;
     }
-    T tempvalue = temp->value;
-    temp->value = temp2->value;
-    temp2->value = tempvalue;
+    
+    // Find the second node to be swapped
+    Node<T>* prev2 = nullptr;
+    Node<T>* curr2 = head;
+    i = 0;
+    while (curr2 != nullptr && i < secondItemIdx) {
+        prev2 = curr2;
+        curr2 = curr2->next;
+        i++;
+    }
+    
+    // If either node is not found, there is nothing to swap
+    if (curr1 == nullptr || curr2 == nullptr) {
+        return;
+    }
+    
+    // Update the next pointers of the previous nodes
+    if (prev1 != nullptr) {
+        prev1->next = curr2;
+    } else {
+        head = curr2;
+    }
+    
+    if (prev2 != nullptr) {
+        prev2->next = curr1;
+    } else {
+        head = curr1;
+    }
+    
+    // Swap the next pointers of the nodes to be swapped
+    Node<T>* temp = curr1->next;
+    curr1->next = curr2->next;
+    curr2->next = temp;
 }
+
 
 template <class T>
 bool SingleLinkedList<T>::isEmpty()
